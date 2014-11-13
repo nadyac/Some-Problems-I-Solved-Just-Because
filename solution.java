778d3239770132c0ba478aa7ac58dfdb36e264e6
nadyac <nadyacpena@gmail.com>
2014-11-12 23:28:42 -0500

    Improved version. Passes all the test cases, however, it is too slow when N is large  (>5k) and times out. Must improve this.

---
+++ solution.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class solution {
+   static String result = "-1";
+	static int numberOf3s = 0;
+	static int numberOf5s = 0;
+	static String concatenated = "";
+	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int tests = input.nextInt();
+		
+		for (int x = 0; x < tests; x++){
+			int N = input.nextInt();
+			
+			//reset all the variables
+			result = "-1";
+			numberOf3s = 0;
+			numberOf5s = 0;
+			concatenated="";
+			
+			if (N < 3){
+				result = "-1";
+			} else if(N == 3) {
+				result = "555";
+			} else if (N == 5){
+				result = "33333";
+			} else if (N > 5){
+				result = subtractFromN(N);
+			}
+			
+			if(result.length() != N){
+				result = "-1";
+			} 
+			System.out.println(result);
+		}
+	}
+		
+		public static String subtractFromN(int N){
+			
+			if (N >= 5 && N % 3 != 0){
+				N = N - 5;
+				numberOf3s = numberOf3s + 5;
+			} 
+			
+			while ( N != 0 && N >= 3){
+				//check if result is divisible by 3
+				if (N % 3 == 0){
+					numberOf5s = numberOf5s + 3*N/3;
+                    N = N - 3*N/3;
+				}
+				//check if result is divisible by 5
+				else if (N % 5 == 0){
+			        numberOf3s = numberOf3s + 5*N/5;
+                    N = N - 5*N/5;
+				} else {
+					String str = subtractFromN(N);
+					return str;
+				}
+			}
+			
+			//generate the decent number
+			for (int i = 0; i < numberOf5s; i++){
+				concatenated = concatenated + 5;
+			}
+			
+			for (int i = 0; i < numberOf3s; i++){
+				concatenated = concatenated + 3;
+			}
+			
+			return concatenated;
+		}
+}
